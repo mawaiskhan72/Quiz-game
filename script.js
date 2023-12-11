038683502ac4187bb3d4d7faa187d99e3f94600e
Muhammad Awais Khan <awaiskhann830@gmail.com>
2023-12-11 10:28:36 -0800

    options ui timmerr are fix on thequiz

--- script.js
+++ script.js
@@ -1,63 +1,72 @@
 const questions = [
   {
     question: "Where is Warsak Dam of Pakistan situated?  ",
-    options: ["a-Khyber Pakhtunkhwa", "b-Punjab", "c-Sindh", "d-Sindh"],
-    answer: "a-Khyber Pakhtunkhwa"
+    options: ["Khyber Pakhtunkhwa", "Punjab", "Sindh", "Balochistan"],
+    answer: "Khyber Pakhtunkhwa"
   },
   {
     question: "Shakarparrian is situated in?",
-    options: ["a-Rawalpindi", "b-Murree", "c-Islamabad", "d-Peshawar"],
-    answer: "c-Islamabad"
+    options: ["Rawalpindi", "Murree", "Islamabad", "Peshawar"],
+    answer: "Islamabad"
   },
   {
     question: "National code of Pakistan is?",
-    options: ["a-Pak", "b-Pk", "c-Pak 1", "d-None of them"],
-    answer: "b-Pk"
+    options: ["Pak", "Pk", "Pak 1", "None of them"],
+    answer: "Pk"
   },
 
   {
     question: "Mirpur is a famous city of?",
-    options: ["a-Punjab", "b-Azad Kashmir", "c-None of them", "d-KPK"],
-    answer: "b-Azad Kashmir"
+    options: ["Punjab", "Azad Kashmir", "None of them", "KPK"],
+    answer: "Azad Kashmir"
   },
   {
     question: "The headquarters of Air Force is located in _________ ?",
-    options: ["a-Islamabad", "b-Peshawar", "c-Rawalpindi", "d-Karachi"],
-    answer: "a-Islamabad"
+    options: ["Islamabad", "Peshawar", "Rawalpindi", "Karachi"],
+    answer: "Islamabad"
   },
   {
     question: "Who is entitled as ” MOHSIN-E- PAKISTAN” ?",
-    options: ["a-Dr Abdul Salam", "b-Dr Adeeb Rizvi", "c-Dr Afia Siddiqui", "d-Dr Abdul Qadeer Khan"],
-    answer: "d-Dr Abdul Qadeer Khan"
+    options: ["Dr Abdul Salam", "Dr Adeeb Rizvi", "Dr Afia Siddiqui", "Dr Abdul Qadeer Khan"],
+    answer: "Dr Abdul Qadeer Khan"
   },
   {
     question: "The war of independence was started in?",
-    options: ["a-1858", "b-1857", "c-1867", "d-1859"],
-    answer: "b-1857"
+    options: ["1858", "1857", "1867", "1859"],
+    answer: "1857"
   },
   {
     question: "The first operational motorway in Pakistan named?",
-    options: ["a-M-1", "b-M-2", "c-M-3", "d-M-4"],
-    answer: "b-M-2"
+    options: ["M-1", "M-2", "M-3", "M-4"],
+    answer: "M-2"
   },
   {
     question: "Qissa khawani Bazar is in which district of Pakistan?",
-    options: ["a-Lahore", "b-Peshawar", "c-Hyderabad", "d-Quetta"],
-    answer: "b-Peshawar"
+    options: ["Lahore", "Peshawar", "Hyderabad", "Quetta"],
+    answer: "Peshawar"
   },
   {
     question: "The land of hospitality is____________?",
-    options: ["a-KPK", "b-Sindh", "c-Baluchistan", "d-Punjab"],
-    answer: "a-KPK"
+    options: ["KPK", "Sindh", "Baluchistan", "Punjab"],
+    answer: "KPK"
   },
 ];
 
 const quizContainer = document.getElementById('quiz-container');
 const submitButton = document.getElementById('submit-btn');
 const finalScoreDisplay = document.getElementById('final-score');
-let finalScore = 0;
-let timeLeft = 60; // Set initial time in seconds
+const timerDisplay = document.getElementById('timer');
 
+let finalScore = 0;
+let timeLeft = 60;
+let timerInterval;
+ 
+
+
+function startQuiz() {
+  displayQuiz(); // Display quiz questions only if the container is empty
+  startTimer(); // Start the timer
+}
 
 function displayQuiz() {
   questions.forEach((quiz, index) => {
@@ -73,77 +82,102 @@ function displayQuiz() {
     questionStatement.style.paddingTop = '10px'; // Padding top for the question
     questionDiv.appendChild(questionStatement);
 
-    if (index < 10) {
-      questionHeading.classList.add('font-semibold');
-    }
+    // Adding radio buttons for options
+    quiz.options.forEach((option, optionIndex) => {
+      const optionContainer = document.createElement('div');
 
-    quiz.options.forEach(option => {
-      const optionButton = document.createElement('button');
-      optionButton.textContent = option;
-      optionButton.classList.add('px-3', 'py-1', 'rounded-xl', 'bg-white');
-      optionButton.addEventListener('click', () => selectAnswer(optionButton, option, quiz.answer));
-      questionDiv.appendChild(optionButton);
+      const optionRadio = document.createElement('input');
+      optionRadio.type = 'radio';
+      optionRadio.name = `question_${index}`; // Assigning same name to group the radio buttons
+      optionRadio.value = option;
+      optionRadio.classList.add('mr-2'); // Adding Tailwind CSS classes
+      optionContainer.appendChild(optionRadio);
+
+      const optionLabel = document.createElement('label');
+      optionLabel.textContent = option;
+      optionContainer.appendChild(optionLabel);
+
+      questionDiv.appendChild(optionContainer);
+
+      // Add line break after each option
+      const lineBreak = document.createElement('br');
+      questionDiv.appendChild(lineBreak);
     });
 
     quizContainer.appendChild(questionDiv);
   });
 }
 
-function selectAnswer(optionButton, selectedOption, correctAnswer) {
-  if (!optionButton.disabled) {
-    if (selectedOption === correctAnswer) {
-      finalScore++;
-    }
-    optionButton.disabled = true;
-  }
-}
-
-function calculateScore() {
-  finalScoreDisplay.textContent = finalScore;
-}
-
-
-let quizCompleted = false;
-let timerInterval; // Variable to store the interval
-
-// Function to handle the timer
-function startTimer() {
-  const timerDisplay = document.getElementById('timer'); // Assuming you have an element with id 'timer' to display the timer
-
-  timerInterval = setInterval(() => {
-    if (!quizCompleted) {
-      timeLeft--;
-      timerDisplay.textContent = `Time left: ${timeLeft} sec`;
-
-      if (timeLeft === 0) {
-        clearInterval(timerInterval); // Stop the timer when it reaches 0
-        timerDisplay.textContent = 'Time is up! Try harder.';
-        disableOptions(); // Disable options when time is up
-      }
-    } else {
-      clearInterval(timerInterval); // Stop the timer when the quiz is completed
-    }
-  }, 1000); // Update the timer every second
-}
-
-function calculateScore() {
-  finalScoreDisplay.textContent = finalScore; // Display the final score
-  quizCompleted = true; // Set quizCompleted to true when the quiz is completed
-  clearInterval(timerInterval); // Stop the timer when the quiz is completed
-}
-
-function disableOptions() {
-  const optionButtons = document.querySelectorAll('button[role="option"]');
-  optionButtons.forEach(button => {
-    button.disabled = true; // Disable all option buttons
-  });
-  submitButton.disabled = true; // Disable submit button after time is up
-}
-
-displayQuiz();
-startTimer();
 submitButton.addEventListener('click', () => {
   calculateScore(); // Calculate and display the final score when the submit button is clicked
 });
 
 
+
+submitButton.addEventListener('click', () => {
+  calculateScore(); // Calculate and display the final score when the submit button is clicked
+});
+
+function calculateScore() {
+  finalScore = 0; // Reset finalScore to recalculate
+
+  questions.forEach((quiz, index) => {
+    const correctAnswer = quiz.answer;
+    const selectedCheckboxes = document.querySelectorAll(`input[type="checkbox"][name='question_${index}']:checked`);
+
+    const selectedOptions = Array.from(selectedCheckboxes).map(checkbox => checkbox.value);
+
+    if (selectedOptions.length === 1 && selectedOptions[0] === correctAnswer) {
+      finalScore++;
+    }
+  });
+
+  finalScoreDisplay.textContent = finalScore; // Display the final score
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timeLeft--;
+
+    if (timeLeft >= 0) {
+      timerDisplay.textContent = `Time left: ${timeLeft} sec`;
+    } else {
+      clearInterval(timerInterval);
+      timerDisplay.textContent = 'Time is up!';
+      calculateScore();
+    }
+  }, 1000);
+}
+
+function startQuiz() {
+  displayQuiz();
+  startTimer();
+}
+
+submitButton.addEventListener('click', () => {
+  clearInterval(timerInterval);
+  calculateScore();
+  displayFinalScore();
+});
+
+function calculateScore() {
+  finalScore = 0;
+  
+  questions.forEach((quiz, index) => {
+    const selectedRadio = document.querySelector(`input[type="radio"][name='question_${index}']:checked`);
+
+    if (selectedRadio && selectedRadio.value === quiz.answer) {
+      finalScore++;
+    }
+  });
+
+  finalScoreDisplay.textContent = finalScore;
+}
+
+function displayFinalScore() {
+  const finalScoreText = document.createElement('p');
+  finalScoreText.textContent = `Final Score: ${finalScore}`;
+}
+
+
+startQuiz();
